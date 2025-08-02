70aa2eec1881e558619c6de008feed833e763fec
Mauricio <mauricio@example.com>
2025-08-02 17:24:26 +0200

    feat: Cambiar routing - Login como página principal, registro como opción secundaria

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { LoginPage } from '../pages/LoginPage';
 import { WelcomePage } from '../pages/WelcomePage';
 import { ProfessionalWorkflowPage } from '../pages/ProfessionalWorkflowPage';
 import { OnboardingPage } from '../pages/OnboardingPage';
@@ -8,7 +9,9 @@ import { AccountActivationPage } from '../pages/AccountActivationPage';
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<WelcomePage />} />
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<WelcomePage />} />
       <Route path="/onboarding" element={<OnboardingPage />} />
       <Route path="/professional-workflow" element={<ProfessionalWorkflowPage />} />
       <Route path="/legal-admin" element={<LegalConsentAdminPage />} />
