d3ad827123e2a0f9a676ce70a682941ee2c152b0
Maurosg78 <143440484+Maurosg78@users.noreply.github.com>
2025-10-19 21:28:24 +0200

    ci(#261): canonical EN pipeline (Lint/E2E/Size + SoT)
    Market: CA
    Language: en-CA
    COMPLIANCE_CHECKED
    Signed-off-by: ROADMAP_READ
    Signed-off-by: SoT-Guardian

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint','react','react-hooks','import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  settings: { react: { version: 'detect' } },
+  ignorePatterns: ['dist','node_modules','**/*.d.ts']
+};
