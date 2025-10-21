86128ed6d305cec2e45529d315811525424ade64
AiduxCare Canonical Pipeline <canonical@aiduxcare.com>
2025-10-21 20:08:29 +0200

    fix: JSX syntax - close LanguageProvider tag
    Signed-off-by: ROADMAP_READ
    COMPLIANCE_CHECKED

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { LanguageProvider } from "./contexts/LanguageContext";import { AuthProvider } from './context/AuthContext';
+import { LanguageProvider } from "./contexts/LanguageContext";
+import { AuthProvider } from './context/AuthContext';
 import { SessionProvider } from './context/SessionContext';
 import AppRouter from './router';
 
@@ -7,9 +8,10 @@ export default function App() {
   return (
     <LanguageProvider>
       <AuthProvider>
-      <SessionProvider>
-        <AppRouter />
-      </SessionProvider>
-    </AuthProvider>
+        <SessionProvider>
+          <AppRouter />
+        </SessionProvider>
+      </AuthProvider>
+    </LanguageProvider>
   );
 }
